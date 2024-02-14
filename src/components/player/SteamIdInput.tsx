f9517b273605c09e05b2f8570ba6200271b3f50b
unknown <mdorfman21@gmail.com>
2024-02-13 22:31:39 -0500

    work steam id route chagne

--- src/components/player/SteamIdInput.tsx
+++ src/components/player/SteamIdInput.tsx
@@ -1,14 +1,24 @@
 import React, { useState } from "react";
 import { Unstable_NumberInput as NumberInput } from "@mui/base/Unstable_NumberInput";
 import { Button } from "@mui/material";
+import PlayerController from "../../controllers/PlayerController.tsx";
+import { useNavigate } from "react-router-dom";
 
 const SteamIdInput = () => {
   const [steamId, setSteamId] = useState(null);
-
+  console.log(steamId);
+  const navigate = useNavigate();
   return (
     <>
-      <NumberInput onChange={(e) => setSteamId(e.target.value)} />
-      <Button>Submit</Button>
+      <NumberInput onChange={(e, val) => setSteamId(val)} value={steamId} />
+      <Button
+        onClick={() => {
+          console.log("click");
+          navigate(`/player/${steamId}`);
+        }}
+      >
+        Submit
+      </Button>
     </>
   );
 };
